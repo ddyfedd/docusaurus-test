ca7a8d08ee495882ae04b0ba00711a86a5092b69
David Zsamboki <david.zsamboki@revolut.com>
2025-01-13 20:22:13 +0100

    update docusaurus config trailingSlash true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ddyfedd', // Usually your GitHub org/user name.
   projectName: 'docusaurus-test', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
