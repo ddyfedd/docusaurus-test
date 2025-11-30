4b8d1ebbb616600c12e660158d201d8f2a176295
Dávid Zsámboki <david.zsamboki@revolut.com>
2025-11-30 02:31:43 +0100

    update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://ddyfedd.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-test/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ddyfedd', // Usually your GitHub org/user name.
-  projectName: 'docusaurus-test', // Usually your repo name.
+  projectName: 'ddyfedd.github.io', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'warn',
